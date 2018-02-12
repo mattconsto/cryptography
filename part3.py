7655ec3f666107c3c70e165c406b13ea5e471c56
Matthew Consterdine <mrconsterdine@googlemail.com>
2018-02-12 15:19:51 +0000

    Work on coursework

--- part3.py
+++ part3.py
@@ -4,5 +4,6 @@
 # ==========
 # Hint: The original message contains alphabetical letters only,
 #       and is encrypted using a two stage crypto-system.
+# Hint: Part Two will help solve part three
 
 ciphertext = "knztzgekbqekiqcevp`kdqaxzpjuknvpvpnqknztgrvpzzhokn~p`{eu`rvtddzzs"
