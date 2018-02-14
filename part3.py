60e620f511b506096027d6719b85fdd21ad4b79f
Matthew Consterdine <mrconsterdine@googlemail.com>
2018-02-14 01:34:31 +0000

    Solve part2

--- part3.py
+++ part3.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
+import re
+
 # Part Three
 # ==========
 # Hint: The original message contains alphabetical letters only,
 #       and is encrypted using a two stage crypto-system.
 # Hint: Part Two will help solve part three
 
-ciphertext = "knztzgekbqekiqcevp`kdqaxzpjuknvpvpnqknztgrvpzzhokn~p`{eu`rvtddzzs"
+ciphertext = bytearray("knztzgekbqekiqcevp`kdqaxzpjuknvpvpnqknztgrvpzzhokn~p`{eu`rvtddzzs".encode("ascii"))
+
+for index in range(2**7):
+	plaintext = bytearray()
+	for character in ciphertext:
+		plaintext.append(127 - character + 96 - index)
+
+	print(plaintext.decode("cp1252"))
