c72858f93604d0cc003bd61701924208af6e0b32
Matthew Consterdine <mrconsterdine@googlemail.com>
2018-02-16 23:03:22 +0000

    Complete part 3

--- part2.py
+++ part2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import codecs, sys
+
 # Part Two
 # ========
 # Hint: The first letter of the original message is A
@@ -11,4 +13,19 @@
 # xor 0x13 = 0b0001_0011
 #   = 0x41 = 0b0100_0001
 
-with open("secret.hex","rb") as f: print(bytearray([c^[0x13,0x20][i%2] for i,c in enumerate(f.read())]).decode("cp1252"))
+if len(sys.argv) == 2:
+	# Decipher using keys from argv and data from stdin
+	keys = list(map(int, sys.argv[1].split(",")))
+	data = sys.stdin.buffer.read()
+	print(bytearray([c^keys[i%len(keys)] for i,c in enumerate(data)]).decode("cp1252"))
+elif len(sys.argv) == 3:
+	# Decipher using keys and filename from argv
+	keys = list(map(int, sys.argv[1].split(",")))
+	with open(sys.argv[2],"rb") as f: data = f.read()
+	print(bytearray([c^keys[i%len(keys)] for i,c in enumerate(data)]).decode("cp1252"))
+else:
+	print("Usage:", sys.argv[0], "KEY1,KEY2,... | KEY1,KEY2,... FILE")
+	# Default to show it working
+	with open("secret.hex","rb") as f: data = f.read()
+	print(bytearray([c^[0x13,0x20][i%2] for i,c in enumerate(data)]).decode("cp1252"))
+	exit(1)
--- part3.py
+++ part3.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-import re
-
-from collections import Counter
+import math, re, sys
 
 # Part Three
 # ==========
@@ -13,13 +11,82 @@ from collections import Counter
 # oddly, all of the characters are in the range ` → ~, which is the last 32 characters in ascii. I wonder if it is important.
 # My guess is some method which converts all of the characters into a printable set of characters, followed by a transposition cipher, but I am probably mistaken.
 
-ciphertext = bytearray("knztzgekbqekiqcevp`kdqaxzpjuknvpvpnqknztgrvpzzhokn~p`{eu`rvtddzzs".encode("ascii"))
+# ciphertext = "knztzgekbqekiqcevp`kdqaxzpjuknvpvpnqknztgrvpzzhokn~p`{eu`rvtddzzs"
+# ciphertext = "PzQeO`JeVdE|X~EpQ~TaPJ{NaX@"
+# print(bytearray(ciphertext.encode("cp1252")))
+# gsvivzivnliveloxzmlvhlmevmfhgszmzmblgsvikozmvgdrgsrmlfihlozihbhgvn
+# therearemorevolcanoesonvenusthananyotherplanetwithinoursolarsystem
 
-plaintext = ""
+def decipher(ciphertext):
+	# Go forever basically.
+	for keycount in range(1, 1000000):
+		found = False
+		print(str(keycount) + ":")
 
-mapping = {122: "e", 107: "t", 112: "a", 110: "o", 118: "i", 101: "n", 113: "s", 116: "r", 96: "h", 100: "d", 103: "l", 117: "u", 114: "c", 98: "m", 105: "f", 99: "y", 97: "w", 120: "g", 106: "p", 104: "b", 111: "v", 126: "k", 123: "x", 127: "q", 115: "j"}
+		for it in range(256**keycount):
+			# Generate keys, then XOR each byte.
+			keys = [math.floor(it / (256 ** i) % 256) for i in range(keycount)]
+			middletext = [chr((ord(c) ^ keys[i%len(keys)]) % 256) for i, c in enumerate(ciphertext)]
+			# Cut down results, to decode more complex strings alter the regex.
+			if re.match(r"[a-z]{"+str(len(ciphertext))+"}", "".join(middletext)):
+				found = True
+				plaintext = [chr(25 - ord(c) + 2*ord("a")) for c in middletext]
+				print(keys, "".join(plaintext))
 
-for character in ciphertext:
-	plaintext += mapping[character]
+		if not found: print("Nothing found.")
 
-print(plaintext)
+		print()
+
+def encipher(keys, plaintext):
+	# Substitute then XOR
+	middletext = [chr(25 - ord(c) + 2*ord("a")) for c in plaintext]
+	plaintext = [chr((ord(c)^keys[i%len(keys)])%256) for i,c in enumerate(middletext)]
+	print("".join(plaintext))
+
+if len(sys.argv) == 2:
+	# Brute force decipher
+	decipher(sys.argv[1])
+elif len(sys.argv) == 3:
+	# Encipher text
+	keys = list(map(int, sys.argv[1].split(",")))
+	encipher(keys, sys.argv[2])
+else:
+	print("Usage:", sys.argv[0], "- | CIPHERTEXT | KEY1,KEY2,... PLAINTEXT")
+	# Default to show it working
+	decipher("knztzgekbqekiqcevp`kdqaxzpjuknvpvpnqknztgrvpzzhokn~p`{eu`rvtddzzs")
+	exit(1)
+
+# search for[a-z]{66}
+
+# gsvivzivnliveloxzmlvhlmevmfhgszmzmblgsvikozmvgdrgsrmlfihlozihbhgvn
+
+# count = Counter(ciphertext)
+# for i in range(96, 128):
+# 	# test = (~(i & 0x1f)) + 0x60
+# 	# print("0b{0:b}".format(test), chr(test), "x" * count[chr(i)])
+# 	print("0b{0:b}".format(i), chr(i), "x" * count[chr(i)])
+# print()
+
+# 0b1100101 e xxxx
+# 0b1111010 z xxxxxxxx
+
+# for c in ciphertext: print(c, "0x{0:b}".format(ord(c)))
+
+# output = ""
+# for c in ciphertext:
+# 	index = 32 - ord(c) + ord("`")
+# 	# print(index)
+# 	output += "       abcdefghijklmnopqrstuvwxyz          "[index]
+
+# print(output)
+
+# for columns in range(1, 10):
+# 	matrix = ["" for _ in range(columns)]
+
+# 	for i, c in enumerate(output):
+# 		matrix[i % columns] += c
+
+# 	for m in matrix:
+# 		print(m)
+
+# 	print()
